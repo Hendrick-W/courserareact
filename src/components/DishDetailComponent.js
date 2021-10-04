38d230ad8585b8fae053901a45fa4912ab9bd68a
Hendrick <hendrickwijaya21@gmail.com>
2021-10-04 16:24:47 +0700

    Functional Components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,9 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class DishDetail extends Component {
 
-    renderComments(comments) {
+    function RenderDish({dish}) {
+        return (
+            <div className="col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg width="100%" src={dish.image}alt={dish.name}/>
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        )
+    }
+
+    function RenderComments({comments}) {
         return comments.map((comment) => {
             return (
                 <li key={comment.id}>
@@ -14,31 +27,23 @@ class DishDetail extends Component {
         })
     }
 
-    render() {
-        const dish = this.props.dish;
+    const DishDetail = (props) => {
+        const dish = props.dish;
 
         if (dish == null) return <div></div>
 
         return (
             <>
-                <div className="col-12 col-md-5 m-1">
-                    <Card>
-                        <CardImg width="100%" src={dish.image}alt={dish.name}/>
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                </div>
+                <RenderDish dish = {dish}/>
                 <div className="col-12 col-md-5 m-1">
                     <h4>Comments</h4>
                     <ul className="list-unstyled">
-                        {this.renderComments(dish.comments)}
+                        <RenderComments comments= {dish.comments}/>
                     </ul>
                 </div>
             </>
         )
     }
-}
+
 
 export default DishDetail;
